94d76722822b3279e0e87cfc0aea16ed941b967c
tiesen243 <ttien56906@gmail.com>
2026-03-27 19:48:53 +0700

    feat: setup web

---
+++ apps/web/src/routes/_index.tsx
@@ -0,0 +1,9 @@
+export default function IndexPage() {
+  return (
+    <main>
+      <h1>
+        Welcome to Remix! This is the index page. You can start editing it in
+      </h1>
+    </main>
+  )
+}
