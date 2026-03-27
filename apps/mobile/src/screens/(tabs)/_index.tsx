8d708ef3df2a8b25acf93d6df3f935ce14f9063b
tiesen243 <ttien56906@gmail.com>
2026-03-28 05:33:06 +0700

    fix(ci): deploy

--- apps/mobile/src/screens/(tabs)/_index.tsx
+++ apps/mobile/src/screens/(tabs)/_index.tsx
@@ -1,19 +1,13 @@
-import { Button } from 'heroui-native/button'
-import { useThemeColor } from 'heroui-native/hooks'
 import { Text } from 'react-native'
 
 import { Container } from '@/components/container'
 
 export default function IndexScreen() {
-  const color = useThemeColor('muted')
-
   return (
     <Container className='items-center justify-center gap-4 px-4' inTab>
-      <Text className='text-2xl font-bold text-accent-hover'>
+      <Text className='text-2xl font-bold text-foreground'>
         Welcome to the Qianyu!
       </Text>
-
-      <Button>{color}</Button>
     </Container>
   )
 }
