0fe13293920e9d45bf7ba8e066d0d98e326e5aca
tiesen243 <ttien56906@gmail.com>
2026-03-28 05:36:34 +0700

    fix(ci): format

--- apps/web/src/routes/_index.tsx
+++ apps/web/src/routes/_index.tsx
@@ -1,11 +1,9 @@
-
 import type { Route } from './+types/_index'
 
 export default function IndexPage(_: Route.ComponentProps) {
   return (
     <main className='container py-4'>
       <h1 className='text-center text-xl font-bold'>Welcome to Qianyu</h1>
-
     </main>
   )
 }
